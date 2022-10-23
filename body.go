266ae5d25f47025580b72f935ca03ff4067602e8
Alexandre Awadallak <alexandre.awadallak@gmail.com>
2022-10-23 14:38:39 -0700

    feat: add body implementation (#9)

---
+++ body.go
@@ -0,0 +1,28 @@
+package httpclient
+
+import "io"
+
+type body struct {
+	raw io.ReadCloser
+}
+
+func (b *body) Raw() io.Reader {
+	return b.raw
+}
+
+func (b *body) Close() error {
+	return b.raw.Close()
+}
+
+func (b *body) Bytes() ([]byte, error) {
+	body, err := io.ReadAll(b.raw)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := b.raw.Close(); err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
