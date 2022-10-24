69684f71261c69cf7591f2918b4eabe6822dfd4a
Alexandre Awadallak <alexandre.awadallak@gmail.com>
2022-10-24 15:10:26 +0000

    add code doc to body

--- body.go
+++ body.go
@@ -6,14 +6,24 @@ type body struct {
 	raw io.ReadCloser
 }
 
+// Raw returns the payload without reading it.
+// The `Close` should be called, otherwise,
+// it's never will be released from the `net/http` pool.
 func (b *body) Raw() io.Reader {
 	return b.raw
 }
 
+// Close release the resource from `net/http`
+// and close the payload.
+//
+// It should be use only with `Raw`.
 func (b *body) Close() error {
 	return b.raw.Close()
 }
 
+// Bytes reads the payload and returns the content.
+//
+// It could return an error while reading or closing the payload
 func (b *body) Bytes() ([]byte, error) {
 	body, err := io.ReadAll(b.raw)
 	if err != nil {
