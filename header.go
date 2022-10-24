be5c1b4fd6b9040165c7b128cc3074fcf6305ad8
Alexandre Awadallak <alexandre.awadallak@gmail.com>
2022-10-24 15:19:35 +0000

    add code doc to header

--- header.go
+++ header.go
@@ -12,6 +12,11 @@ type header struct {
 
 var _ Header = (*header)(nil)
 
+// Add adds the key, value pair to the header.
+// It appends to any existing values associated with key.
+// The key is case insensitive; it is canonicalized by CanonicalHeaderKey.
+//
+// It's concurrent safe to add keys to the header.
 func (h *header) Add(key string, value string) {
 	h.mu.Lock()
 	defer h.mu.Unlock()
@@ -19,6 +24,11 @@ func (h *header) Add(key string, value string) {
 	h.metadata.Add(key, value)
 }
 
+// Set sets the header entries associated with key to the single element value.
+// It replaces any existing values associated with key.
+// The key is case insensitive; it is canonicalized by textproto.CanonicalMIMEHeaderKey.
+//
+// It's concurrent safe to set keys to the header.
 func (h *header) Set(key string, value string) {
 	h.mu.Lock()
 	defer h.mu.Unlock()
@@ -26,13 +36,10 @@ func (h *header) Set(key string, value string) {
 	h.metadata.Set(key, value)
 }
 
-func (h *header) Values() map[string][]string {
-	h.mu.RLock()
-	defer h.mu.RUnlock()
-
-	return h.metadata.Clone()
-}
-
+// Del deletes the values associated with key.
+// The key is case insensitive; it is canonicalized by CanonicalHeaderKey.
+//
+// It's concurrent safe to delete keys from the header.
 func (h *header) Delete(key string) {
 	h.mu.Lock()
 	defer h.mu.Unlock()
@@ -40,6 +47,21 @@ func (h *header) Delete(key string) {
 	h.metadata.Del(key)
 }
 
+// Values return a copy from the header content.
+//
+// It's concurrent safe to get values from the header.
+func (h *header) Values() map[string][]string {
+	h.mu.RLock()
+	defer h.mu.RUnlock()
+
+	return h.metadata.Clone()
+}
+
+// Get gets the first value associated with the given key.
+// If there are no values associated with the key, Get returns an empty string.
+// It is case insensitive; textproto.CanonicalMIMEHeaderKey is used to canonicalize the provided key.
+//
+// It's concurrent safe to get a value from the header.
 func (h *header) Get(key string) (string, bool) {
 	h.mu.RLock()
 	defer h.mu.RUnlock()
